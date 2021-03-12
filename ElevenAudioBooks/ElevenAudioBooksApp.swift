4ac550633d3a037bed8c37eb245c3e024777d62e
H6nry <henry.anonym@gmail.com>
2021-03-12 08:33:38 +0100

    Initial Commit

---
+++ ElevenAudioBooks/ElevenAudioBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevenAudioBooksApp.swift
+//  ElevenAudioBooks
+//
+//  Created by Henry Greschniok on 12.03.21.
+//
+
+import SwiftUI
+
+@main
+struct ElevenAudioBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
