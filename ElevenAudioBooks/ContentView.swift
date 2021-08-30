38fc8b7530f2cc484ca78a0c033f559b09e28d9a
H6nry <henry.anonym@gmail.com>
2021-08-30 09:54:00 +0200

    First commit in ElevenAudioBooks

--- ElevenAudioBooks/ContentView.swift
+++ ElevenAudioBooks/ContentView.swift
@@ -2,20 +2,362 @@
 //  ContentView.swift
 //  ElevenAudioBooks
 //
-//  Created by Henry Greschniok on 12.03.21.
+//  Created by H6nry on 12.03.21.
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+
+
+enum EditFieldEditType {
+	case name
+	case year
+	case genre
+	case artist
+	case t_title
+	case t_trackNr
+	case t_year
+	case t_genre
+	case t_artist
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+
+
+struct ContentView: View {
+	@ObservedObject var viewModel: ViewModel
+	@Binding var selectedBook: UUID?
+	@Binding var selectedItem: UUID?
+	
+	@State private var saveChangesWarningPresented: Bool = false
+	
+    var body: some View {
+		VStack {
+			NavigationView {
+				let booksList:Array<AudioBook> = viewModel.audioBooks.map { $0.value }
+				
+				List(booksList, id: \.id) { book in
+					NavigationLink(destination: AudioBookDetailView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem), tag: book.id, selection: $selectedBook) {
+						Text(book.name)
+					}
+				}.listStyle(SidebarListStyle())
+				
+				Text("AudioBook")
+					.frame(minWidth: 100, idealWidth: 150, maxWidth: 200, maxHeight: .infinity)
+					//.background(Color(red: 222/255, green: 228/255, blue: 234/255, opacity: 1))
+				Text("Items")
+					.frame(minWidth: 100, idealWidth: 150, maxWidth: 200, maxHeight: .infinity)
+				Text("Edit Item")
+					.frame(minWidth: 100, idealWidth: 150, maxWidth: 200, maxHeight: .infinity)
+			}.frame(minWidth: 800, maxWidth: .infinity, minHeight: 500, maxHeight: .infinity)
+			.toolbar() {
+				ToolbarItem(placement: ToolbarItemPlacement.navigation) {
+					Button(action: {
+						NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+					}, label: {
+						Image(systemName: "sidebar.leading")
+					}).help("Toggle Side Bar")
+					Spacer()
+				}
+				
+				ToolbarItem() {
+					Button(action: {
+						viewModel.reload()
+					}, label: {
+						Image(systemName: "arrow.triangle.2.circlepath")
+					}).help("Discard changes and reload from Database")
+				}
+				
+				ToolbarItem() {
+					Button(action: {
+						saveChangesWarningPresented = true
+					}, label: {
+						Image(systemName: "tray.and.arrow.down.fill")
+					})
+					.help("Save changes to Library")
+					.alert(isPresented: $saveChangesWarningPresented, content: {
+						Alert(title: Text("Warning"), message: Text("ibooks-library-delete-kill-warning"), primaryButton: .destructive(Text("I have a Backup. Proceed.")) {
+							viewModel.saveToLibrary()
+						}, secondaryButton: .cancel())
+					})
+				}
+			}
+		}
+	}
+}
+
+
+struct AudioBookDetailView: View {
+	@ObservedObject var viewModel: ViewModel
+	@Binding var selectedBook: UUID?
+	@Binding var selectedItem: UUID?
+	@State private var isEditNamePopoverPresented: Bool = false
+	
+	
+	var body: some View {
+		//NavigationView {
+			List {
+				
+				/*Button("Edit") {
+					viewModel.audioBooksList[0].name = "DISODASO"
+					selectedBook = viewModel.audioBooksList[0]
+					//self.isEditNamePopoverPresented = true
+				}*//*.popover(isPresented: self.$isEditNamePopoverPresented) {
+					Text("Popover is Presented").frame(width: 500, height: 500)
+				}*/
+				/*HStack {
+					Text("Name: ")
+					TextField("Name", text: name)
+				}*/
+				EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.name)
+				EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.artist)
+				EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.genre)
+				EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.year)
+				
+				NavigationLink(destination: AudioBookItemsView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem)) {
+					HStack {
+						Text("Tracks")
+						Spacer()
+						Image(systemName: "chevron.forward").frame(alignment: Alignment.trailing)
+					}
+				}
+			}
+		//}
+	}
+}
+
+
+struct AudioBookItemsView: View {
+	@ObservedObject var viewModel: ViewModel
+	@Binding var selectedBook: UUID?
+	@Binding var selectedItem: UUID?
+	//@State var itemsList: Array<AudioBookTrack> = []
+	
+	var body: some View {
+		/*let itemsListu:Array<AudioBookTrack> = viewModel.audioBooks[selectedBook!]!.items.map { $0.value }
+		let itemsList:Array<AudioBookTrack> = itemsListu.sorted(by: {$0.trackNr < $1.trackNr})*/
+		let itemsList: Array<AudioBookTrack> = viewModel.audioBooks[selectedBook!]!.itemsSortedByTrackNrThenName
+		
+			List(itemsList, id: \.id) { item in
+				NavigationLink(destination: AudioBookItemDetailView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem), tag: item.id, selection: $selectedItem) {
+					HStack {
+						Text(String(item.trackNr) + ". " + item.trackTitle).frame(maxHeight: 50)
+						Spacer()
+						Image(systemName: "chevron.forward").frame(alignment: Alignment.trailing)
+					}
+				}
+			}/*.onAppear() {
+			let itemsListu:Array<AudioBookTrack> = viewModel.audioBooks[selectedBook!]!.items.map { $0.value }
+			itemsList = itemsListu.sorted(by: {$0.trackNr < $1.trackNr})
+			}*/
+	}
+}
+
+struct AudioBookItemDetailView: View {
+	@ObservedObject var viewModel: ViewModel
+	@Binding var selectedBook: UUID?
+	@Binding var selectedItem: UUID?
+	
+	var body: some View {
+		List {
+			EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.t_trackNr)
+			EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.t_title)
+			EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.t_year)
+			EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.t_genre)
+			EditFieldView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem, editFieldType: EditFieldEditType.t_artist)
+		}
+	}
+}
+
+struct EditFieldView: View {
+	@ObservedObject var viewModel: ViewModel
+	@Binding var selectedBook: UUID?
+	@Binding var selectedItem: UUID?
+	let editFieldType: EditFieldEditType
+	
+	var body: some View {
+		let name = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil) {
+				return book!.name
+			}
+			
+			return "UNNAMED"
+		}, set: {
+			let boo = selectedBook
+			viewModel.audioBooks[selectedBook!]?.name = $0
+			selectedBook = boo
+		})
+		
+		let artist = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil) {
+				return book!.artist
+			}
+			
+			return ""
+		}, set: {
+			let boo = selectedBook
+			viewModel.audioBooks[selectedBook!]?.artist = $0
+			selectedBook = boo
+		})
+		
+		let genre = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil) {
+				return book!.genre
+			}
+			
+			return ""
+		}, set: {
+			let boo = selectedBook
+			viewModel.audioBooks[selectedBook!]?.genre = $0
+			selectedBook = boo
+		})
+		
+		let year = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil) {
+				return book!.year
+			}
+			
+			return ""
+		}, set: {
+			let boo = selectedBook
+			viewModel.audioBooks[selectedBook!]?.year = $0
+			selectedBook = boo
+		})
+		
+		
+		let t_trackNr = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil && selectedItem != nil) {
+				let item = book?.items[selectedItem!]
+				if (item != nil) {
+					return String(item!.trackNr)
+				}
+			}
+			return "0"
+		}, set: {
+			let boo = selectedBook
+			let ite = selectedItem
+			if (boo != nil && ite != nil) {
+				viewModel.audioBooks[selectedBook!]?.items[selectedItem!]?.trackNr = Int($0) ?? 0
+			}
+			selectedBook = boo
+			selectedItem = ite
+		})
+		
+		let t_title = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil && selectedItem != nil) {
+				let item = book?.items[selectedItem!]
+				if (item != nil) {
+					return item!.trackTitle
+				}
+			}
+			return ""
+		}, set: {
+			if (selectedBook != nil && selectedItem != nil) {
+				let boo = selectedBook
+				let ite = selectedItem
+				viewModel.audioBooks[selectedBook!]?.items[selectedItem!]?.trackTitle = $0
+				selectedBook = boo
+				selectedItem = ite
+			}
+		})
+		
+		let t_year = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil && selectedItem != nil) {
+				let item = book?.items[selectedItem!]
+				if (item != nil) {
+					return item!.year
+				}
+			}
+			return ""
+		}, set: {
+			let boo = selectedBook
+			let ite = selectedItem
+			viewModel.audioBooks[selectedBook!]?.items[selectedItem!]?.year = $0
+			selectedBook = boo
+			selectedItem = ite
+		})
+		
+		let t_genre = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil && selectedItem != nil) {
+				let item = book?.items[selectedItem!]
+				if (item != nil) {
+					return item!.genre
+				}
+			}
+			return ""
+		}, set: {
+			let boo = selectedBook
+			let ite = selectedItem
+			viewModel.audioBooks[selectedBook!]?.items[selectedItem!]?.genre = $0
+			selectedBook = boo
+			selectedItem = ite
+		})
+		
+		let t_artist = Binding<String>(get: {
+			let book = viewModel.audioBooks[selectedBook!]
+			if (book != nil && selectedItem != nil) {
+				let item = book?.items[selectedItem!]
+				if (item != nil) {
+					return item!.artist
+				}
+			}
+			return ""
+		}, set: {
+			let boo = selectedBook
+			let ite = selectedItem
+			viewModel.audioBooks[selectedBook!]?.items[selectedItem!]?.artist = $0
+			selectedBook = boo
+			selectedItem = ite
+		})
+		
+		
+		HStack {
+			switch editFieldType {
+			case .name:
+				Text("Name:")
+				TextField("Name", text: name)
+			case .year:
+				Text("Year:")
+				TextField("Year", text: year)
+			case .genre:
+				Text("Genre:")
+				TextField("Genre", text: genre)
+			case .artist:
+				Text("Artist:")
+				TextField("Artist", text: artist)
+			case .t_title:
+				Text("Title:")
+				TextField("Title", text:t_title)
+			case .t_trackNr:
+				Text("Track No.:")
+				TextField("Track No.", text:t_trackNr)
+			case .t_year:
+				Text("Year:")
+				TextField("Year", text:t_year)
+			case .t_genre:
+				Text("Genre:")
+				TextField("Genre", text:t_genre)
+			case .t_artist:
+				Text("Artist:")
+				TextField("Artist", text:t_artist)
+			}
+		}.contextMenu {
+			Button("Apply to all titles in audiobook") {
+					print("TODO!!!")
+			}.disabled(!((editFieldType == .artist) || (editFieldType == .year) || (editFieldType == .genre)))
+		}
+	}
+}
+
+struct PlayerView: View {
+	var body: some View {
+		Text("hh") //TODO
+	}
 }
--- ElevenAudioBooks/ElevenAudioBooksApp.swift
+++ ElevenAudioBooks/ElevenAudioBooksApp.swift
@@ -2,16 +2,311 @@
 //  ElevenAudioBooksApp.swift
 //  ElevenAudioBooks
 //
-//  Created by Henry Greschniok on 12.03.21.
+//  Created by H6nry on 12.03.21.
 //
 
 import SwiftUI
 
+struct AudioBookTrack: Identifiable {
+	let id = UUID()
+	var trackTitle: String
+	var year: String
+	var bkGeneratedItemID: String
+	var artist: String
+	var genre: String
+	let path: String
+	var trackNr: Int
+	let plistSkeleton: Dictionary<String, Any>
+}
+
+struct AudioBook: Identifiable {
+	let id = UUID()
+	var name: String
+	var year: String
+	var bkGeneratedItemID: String
+	var artist: String
+	var genre: String
+	var items: Dictionary<UUID, AudioBookTrack>
+	let plistSkeleton: Dictionary<String, Any>
+	var itemsSortedByTrackNrThenName: Array<AudioBookTrack> {
+		get {
+			let itemsListu:Array<AudioBookTrack> = self.items.map { $0.value }
+			return itemsListu.sorted(by: {
+				if ($0.trackNr != $1.trackNr) {
+					return $0.trackNr < $1.trackNr
+				} else {
+					return $0.trackTitle.lowercased() < $1.trackTitle.lowercased()
+				}
+			})
+		}
+	}
+}
+
+class ViewModel: ObservableObject {
+	@Published var audioBooks:Dictionary<UUID, AudioBook> = [:]
+	private var booksPlistSkeleton: Dictionary<String, Any> = [:]
+	
+	
+	init() {
+		print("hello?")
+		/*
+		//Experimental stuff....
+		let connection = NSXPCConnection(machServiceName: "com.apple.BKAgentService", options: NSXPCConnection.Options(rawValue: 0))
+		connection.remoteObjectInterface = NSXPCInterface(with: BKAgentService.self)
+		connection.resume()
+		
+		let service = connection.remoteObjectProxyWithErrorHandler { error in
+			  print("Received error:", error)
+			} as? BKAgentService
+		
+		
+		service!.fetchBooks({(obj:Any, e:Error) -> Void in
+			print(obj)
+			print(e)
+		})*/
+		
+		let homeURL = FileManager.default.homeDirectoryForCurrentUser
+		let booksLibraryURL = homeURL.appendingPathComponent("Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/", isDirectory: true)
+		let booksPlistURL = booksLibraryURL.appendingPathComponent("Books.plist")
+		
+		//First, make a backup file of the library!
+		let booksPlistBackupURL = booksLibraryURL.appendingPathComponent("Books.plist.backup")
+		do {
+			if FileManager.default.fileExists(atPath: booksPlistBackupURL.path) {
+				try FileManager.default.removeItem(at: booksPlistBackupURL)
+			}
+			try FileManager.default.copyItem(at: booksPlistURL, to: booksPlistBackupURL)
+		} catch {
+			print("Backup failed!!!")
+			exit(1)
+		}
+		
+		//Read File into a dictionary
+		var bplist: Dictionary<String, Any> = [:]
+		
+		do {
+			let infoPlistData = try Data(contentsOf: booksPlistURL)
+			
+			if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? Dictionary<String, Any> {
+				bplist = dict
+			}
+		} catch {
+			print(error)
+		}
+		
+		let books:[Dictionary] = bplist["Books"] as! [Dictionary<String, Any>]
+		var booksv:Array<Dictionary<String, Any>> = [] //The part of books which won't be processed by ElevenAudioBooks
+		
+		for book in books {
+			let type = book["BKBookType"] as! String
+			if type == "audiobook" {
+				let name = book["itemName"] as! String
+				let year = book["year"] as! String
+				let id = book["BKGeneratedItemId"] as! String
+				let artist = book["artistName"] as! String
+				let genre = book["genre"] as! String
+				
+				// Remove all indexed keys and values, to store the rest for later
+				var bookv = book
+				bookv.removeValue(forKey: "itemName")
+				bookv.removeValue(forKey: "year")
+				bookv.removeValue(forKey: "BKGeneratedItemId")
+				bookv.removeValue(forKey: "artistName")
+				bookv.removeValue(forKey: "genre")
+				
+				var items:Dictionary<UUID, AudioBookTrack> = [:]
+				let bkparts = book["BKParts"] as! Array<Dictionary<String, Any>>
+				
+				for item in bkparts {
+					let trackTitle = item["BKTrackTitle"] as! String
+					let year = item["year"] as! String
+					let bkGeneratedItemID = item["BKGeneratedItemId"] as! String
+					let artist = item["artistName"] as! String
+					let genre = item["genre"] as! String
+					let path = item["path"] as! String
+					let tracknr = item["BKTrackNumber"] as! Int
+					
+					// Remove all indexed keys and values, to store the rest for later
+					var itemv = item
+					itemv.removeValue(forKey: "BKTrackTitle")
+					itemv.removeValue(forKey: "year")
+					itemv.removeValue(forKey: "BKGeneratedItemId")
+					itemv.removeValue(forKey: "artistName")
+					itemv.removeValue(forKey: "genre")
+					itemv.removeValue(forKey: "path")
+					itemv.removeValue(forKey: "BKTrackNumber")
+					
+					let track = AudioBookTrack(trackTitle: trackTitle, year: year, bkGeneratedItemID: bkGeneratedItemID, artist: artist, genre: genre, path: path, trackNr: tracknr, plistSkeleton: itemv)
+					
+					items[track.id] = track
+				}
+				
+				
+				let audiobook = AudioBook(name: name, year: year, bkGeneratedItemID: id, artist: artist, genre: genre, items: items, plistSkeleton: bookv)
+				/*let audiobook = AudioBook()
+				audiobook.name = name
+				audiobook.year = year
+				audiobook.bkGeneratedItemID = id
+				audiobook.artist = artist
+				audiobook.genre = genre
+				audiobook.items = items*/
+				
+				audioBooks[audiobook.id] = audiobook
+			} else {
+				booksv.append(book)
+			}
+		}
+		
+		//Put the unused books into the plist dict, removing everything else
+		bplist["Books"] = booksv
+		booksPlistSkeleton = bplist
+		//print(booksPlistSkeleton)
+		
+		
+		
+		//Get values, modify, merge, etc.
+		/*var books:[Dictionary] = bplist?["Books"] as! [Dictionary<String, Any>]
+		print(books[11]["itemName"] as Any)
+		books[11]["itemName"] = "Döner Schäfchentangol"
+		
+		
+		bplist!["Books"] = books
+		
+		//Write the dictionary back to file
+		do {
+			let dataToWrite = try PropertyListSerialization.data(fromPropertyList: bplist ?? "", format: PropertyListSerialization.PropertyListFormat.binary, options: 0)
+			try dataToWrite.write(to: booksPlistURL)
+		} catch {
+			print("error writing file")
+		}*/
+	}
+	
+	func saveToLibrary() -> Void {
+		print("saving to library...")
+		var skeleton = booksPlistSkeleton
+		
+		var booksPlist:Array<Dictionary<String, Any>> = []
+		let books:Array<AudioBook> = audioBooks.map { $0.value }
+		
+		for book in books {
+			var dict:Dictionary<String, Any> = book.plistSkeleton
+			
+			dict["itemName"] = book.name
+			dict["year"] = book.year
+			dict["BKGeneratedItemId"] = book.bkGeneratedItemID
+			dict["artistName"] = book.artist
+			dict["genre"] = book.genre
+			
+			var itemsPlist:Array<Dictionary<String, Any>> = []
+			let items: Array<AudioBookTrack> = book.itemsSortedByTrackNrThenName
+			
+			for item in items {
+				var idict:Dictionary<String, Any> = item.plistSkeleton
+				
+				idict["BKTrackTitle"] = item.trackTitle
+				idict["year"] = item.year
+				idict["BKGeneratedItemId"] = item.bkGeneratedItemID
+				idict["artistName"] = item.artist
+				idict["genre"] = item.genre
+				idict["path"] = item.path
+				idict["BKTrackNumber"] = item.trackNr
+				
+				itemsPlist.append(idict)
+			}
+			
+			dict["BKParts"] = itemsPlist
+			booksPlist.append(dict)
+		}
+		var skeletonBooks:Array<Any> = skeleton["Books"] as! Array<Any>
+		skeletonBooks.append(contentsOf: booksPlist)
+		
+		skeleton["Books"] = skeletonBooks
+		
+		//print(skeleton)
+		
+		killiBooksXandRemoveiBooksXSQL()
+		
+		let homeURL = FileManager.default.homeDirectoryForCurrentUser
+		let booksLibraryURL = homeURL.appendingPathComponent("Library/Containers/com.apple.BKAgentService/Data/Documents/iBooks/Books/", isDirectory: true)
+		let booksPlistURL = booksLibraryURL.appendingPathComponent("Books.plist")
+		
+		do {
+			let dataToWrite = try PropertyListSerialization.data(fromPropertyList: skeleton, format: PropertyListSerialization.PropertyListFormat.binary, options: 0)
+			try dataToWrite.write(to: booksPlistURL)
+		} catch {
+			print("error writing file")
+		}
+		
+		
+	}
+	
+	func reload() -> Void {
+		print("reloading... todo") //TODO
+	}
+	
+	func killiBooksXandRemoveiBooksXSQL() -> Void {
+		shell("killall Books")
+		shell("PROZID=$(pgrep com.apple.BKAgentService); kill -9 $PROZID")
+		
+		let homeURL = FileManager.default.homeDirectoryForCurrentUser
+		let iBooksXLibraryURL = homeURL.appendingPathComponent("Library/Containers/com.apple.iBooksX/Data/Documents/", isDirectory: true)
+		let booksLibraryURL = iBooksXLibraryURL.appendingPathComponent("BKLibrary/", isDirectory: true)
+		
+		//First, make a backup
+		let booksLibraryBackupURL = iBooksXLibraryURL.appendingPathComponent("BKLibrary_Backup/", isDirectory: true)
+		do {
+			if FileManager.default.fileExists(atPath: booksLibraryURL.path) {
+				if FileManager.default.fileExists(atPath: booksLibraryBackupURL.path) {
+					try FileManager.default.removeItem(at: booksLibraryBackupURL)
+				}
+				
+				try FileManager.default.moveItem(at: booksLibraryURL, to: booksLibraryBackupURL)
+			}
+		} catch {
+			print("Backup and/or Move failed!!!   ")
+			print(error)
+			exit(1)
+		}
+	}
+	
+	@discardableResult
+	private func shell(_ command: String) -> String {
+		let task = Process()
+		let pipe = Pipe()
+		
+		task.standardOutput = pipe
+		task.standardError = pipe
+		task.arguments = ["-c", command]
+		task.launchPath = "/bin/zsh"
+		task.launch()
+		
+		let data = pipe.fileHandleForReading.readDataToEndOfFile()
+		let output = String(data: data, encoding: .utf8)!
+		
+		return output
+	}
+}
+
+
 @main
 struct ElevenAudioBooksApp: App {
+	@StateObject var viewModel  = ViewModel()
+	@State private var selectedBook: UUID? = UUID()
+	@State private var selectedItem: UUID? = UUID()
+	
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+			ContentView(viewModel: viewModel, selectedBook: $selectedBook, selectedItem: $selectedItem)
+		}.commands {
+			SidebarCommands()
+			CommandGroup(after: CommandGroupPlacement.saveItem, addition: {
+				Button("Save to Library") {
+					viewModel.saveToLibrary()
+				}
+				Button("Reload from Library") {
+					viewModel.reload()
+				}
+			})
+		}
     }
 }
